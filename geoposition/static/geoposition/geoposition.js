8ac42db1d8ecc7aaa43b8e74ab57978d917a7ea3
Tiago Almeida <tiago.b.almeida@gmail.com>
2015-04-29 22:20:37 +0100

    Do not create address input field, use an existing one

--- geoposition/static/geoposition/geoposition.js
+++ geoposition/static/geoposition/geoposition.js
@@ -33,8 +33,10 @@ if (jQuery != undefined) {
                 $mapContainer = $('<div class="geoposition-map" />'),
                 $addressRow = $('<div class="geoposition-address" />'),
                 $searchRow = $('<div class="geoposition-search" />'),
-                $searchInput = $('<input>', {'type': 'search', 'placeholder': 'Start typing an address …'}),
-                $latitudeField = $container.find('input.geoposition:eq(0)'),
+                //changes
+		//$searchInput = $('<input>', {'type': 'search', 'placeholder': 'Start typing an address …'}),
+                $searchInput = $('#id_address'),
+		$latitudeField = $container.find('input.geoposition:eq(0)'),
                 $longitudeField = $container.find('input.geoposition:eq(1)'),
                 latitude = parseFloat($latitudeField.val()) || null,
                 longitude = parseFloat($longitudeField.val()) || null,
@@ -119,7 +121,8 @@ if (jQuery != undefined) {
             }).on('abort', function() {
                 $(this).parent().find('ul.geoposition-results').remove();
             });
-            $searchInput.appendTo($searchRow);
+            //changes
+	    //$searchInput.appendTo($searchRow);
             $container.append($searchRow, $mapContainer, $addressRow);
 
             mapLatLng = new google.maps.LatLng(latitude, longitude);
